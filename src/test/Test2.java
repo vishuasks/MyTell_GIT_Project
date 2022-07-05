c2fda33f9b805672167932370ae4fb4f614027fa
vishuasks <vishwanathasks@gmail.com>
2022-07-05 21:00:24 +0530

    New line has been added in Test2 java

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Second test folder created by x");
+		
+		System.out.println("New line has been added to Test 2 java by X");
 
 	}
 
