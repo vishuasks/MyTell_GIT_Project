1695e79e0f893dc0d20d55dfbe729d1e98eb0d9e
vishuasks <vishwanathasks@gmail.com>
2022-07-05 20:35:26 +0530

    new java file Test2 has been added by x

---
+++ src/test/Test2.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second test folder created by x");
+
+	}
+
+}
